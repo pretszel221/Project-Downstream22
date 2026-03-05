534cd3b18be6ba7ec322b468fab712b83351daab
nottal <110141701+nottal@users.noreply.github.com>
2026-03-04 21:51:37 -0800

    Extra

---
+++ Content.Shared/CCVar/CCVars.Movement.cs
@@ -0,0 +1,15 @@
+using Content.Shared.Administration;
+using Content.Shared.CCVar.CVarAccess;
+using Robust.Shared.Configuration;
+
+namespace Content.Shared.CCVar;
+
+public sealed partial class CCVars
+{
+    /// <summary>
+    /// Is crawling enabled.
+    /// </summary>
+    [CVarControl(AdminFlags.VarEdit)]
+    public static readonly CVarDef<bool> MovementCrawling =
+        CVarDef.Create("movement.crawling", true, CVar.SERVER | CVar.REPLICATED);
+}
